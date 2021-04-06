8df1c4389e8e07641ff7bfcd1c587c8d7382a422
Jim <jim@jekl.com>
2021-04-06 12:07:51 +0000

    updating run.py

--- app/run.py
+++ app/run.py
@@ -91,23 +91,6 @@ def go():
         classification_result=classification_results
     )
 
-# handles user query and displays results
-@app.route('/go')
-def go():
-    # save user input in query
-    query = request.args.get('query', '') 
-
-    # use model to predict classification for query
-    classification_labels = model.predict([query])[0]
-    classification_results = dict(zip(df.columns[4:], classification_labels))
-
-    # This will render the go.html Please see that file. 
-    return render_template(
-        'go.html',
-        query=query,
-        classification_result=classification_results
-    )
-
 def main():
     app.run(host='0.0.0.0', port=3001, debug=True)
 
